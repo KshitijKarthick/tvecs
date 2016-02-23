6277bde1bf24dc1cae27e919f91197293553466a
Kshitij Karthick <kshitij.karthick@gmail.com>
2016-02-23 21:41:46 +0530

    Model Generation seperated from preprocess in model_generation.py

--- model_generation.py
+++ model_generation.py
@@ -4,7 +4,7 @@ import gensim
 from nltk.tokenize import sent_tokenize
 
 
-def preprocess_corpus(language, corpus_filename, output_dir='./models'):
+def preprocess_corpus(language, corpus_filename):
     print "Preprocessing %s corpus => %s" %(language, corpus_filename)
     corpus_data = codecs.open(corpus_filename, mode='r', encoding='utf-8').read()
     tab_split_list = corpus_data.split('\t')
@@ -14,12 +14,18 @@ def preprocess_corpus(language, corpus_filename, output_dir='./models'):
     corpus_body = " ".join(corpus_body_list)
     sent_split_list = sent_tokenize(corpus_body)
     word_sent_split_list = [x.split() for x in sent_split_list]
+    return word_sent_split_list
+    
+
+def generate_model(language, corpus_filename, output_dir='./models'):
+    word_sent_split_list = preprocess_corpus(language, corpus_filename)
     model = gensim.models.Word2Vec(word_sent_split_list)
     output_fname = os.path.join(output_dir, 't-vex-%s-model' %(language))
     model.save(output_fname)
     print "Model saved at %s" %(output_fname)
 
+
 if __name__ == '__main__':
-    preprocess_corpus('hindi', './corpus/Hindi/all.txt')
-    preprocess_corpus('kannada', './corpus/Kannada/all.txt')
-    preprocess_corpus('tamil', './corpus/Tamil/all.txt')
+    generate_model('hindi', './corpus/Hindi/all.txt')
+    generate_model('kannada', './corpus/Kannada/all.txt')
+    generate_model('tamil', './corpus/Tamil/all.txt')
