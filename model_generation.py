79add17d5ea218443de272b2a2ef336965834466
Kshitij Karthick <kshitij.karthick@gmail.com>
2016-03-07 23:35:50 +0530

    model_generation: Script used to generate models based on HcCorpusPreprocessor

--- model_generation.py
+++ model_generation.py
@@ -1,41 +1,77 @@
+"""
+Used to generate Word2Vec Models for individual languages after preprocessing.
+
+* Preprocessing Corpus - Implementation of BasePreprocessor module
+    * HcCorpusPreprocessor
+* Word2Vec Model Building
+    * Gensim Word2Vec SkipGram implementation
+"""
+
 import os
-import string
-import codecs
 import gensim
-from nltk.tokenize import sent_tokenize
+from hccorpus_preprocessor import HcCorpusPreprocessor
 
 
-def preprocess_corpus(language, corpus_filename):
-    print "Preprocessing %s corpus => %s" %(language, corpus_filename)
-    corpus_data = codecs.open(corpus_filename, mode='r', encoding='utf-8').read()
-    tab_split_list = corpus_data.split('\t')
-    del(corpus_data)
-    corpus_body_list = (
-        tab_split_list[x] for x in range(len(tab_split_list)) if x%4==0 and x!=0 
+def generate_model(
+    language,
+    corpus_fname,
+    corpus_dir_path='.',
+    output_dir_path='./models',
+    need_preprocessing=True,
+    iter=5,
+):
+    """
+    Function used to preprocess and generate models.
+
+    Function Parameters:
+    * language           - (string)  - Language for which model is generated
+                                       [ Used for model filename ]
+    * corpus_fname       - (string)  - Corpus Filename
+    * corpus_dir_path    - (string)  - Directory Path where corpus exists
+                                       [ Default current directory ]
+    * output_dir_path    - (string)  - Output Dir Path where model is stored
+    * need_preprocessing - (boolean) - Runs Preprocess with the same flag
+                                       [ Default True ]
+    * iter               - (number)  - Number of iterations for Word2Vec
+
+    """
+    preprocessor_obj = HcCorpusPreprocessor(
+        corpus_fname=corpus_fname,
+        corpus_dir_path=corpus_dir_path,
+        need_preprocessing=True
     )
-    corpus_body = " ".join(corpus_body_list)
-    del(corpus_body_list)
-    sent_split_list = sent_tokenize(corpus_body)
-    del(corpus_body)
-    intermediate = (
-        (sentence.encode('utf-8').translate(None, string.punctuation)).decode('utf-8') for sentence in sent_split_list
-    )
-    del(sent_split_list)
-    word_sent_split_list = [x.split() for x in intermediate]
-    del(intermediate)
-    return word_sent_split_list
-
-
-def generate_model(language, corpus_filename, output_dir='./models', iter=5):
-    word_sent_split_list = preprocess_corpus(language, corpus_filename)
-    model = gensim.models.Word2Vec(word_sent_split_list, iter=iter)
-    output_fname = os.path.join(output_dir, 't-vex-%s-model' %(language))
+    model = gensim.models.Word2Vec(preprocessor_obj, iter=iter)
+    output_fname = os.path.join(output_dir_path, 't-vex-%s-model' % (language))
     model.save(output_fname)
-    print "Model saved at %s" %(output_fname)
+    print("Model saved at %s" % (output_fname))
 
 
 if __name__ == '__main__':
-    generate_model('hindi', './corpus/Hindi/all.txt', iter=30)
-    generate_model('kannada', './corpus/Kannada/all.txt', iter=30)
-    generate_model('tamil', './corpus/Tamil/all.txt', iter=30)
-    generate_model('english', './corpus/English/all.txt', iter=30)
+    generate_model(
+        language='hindi',
+        corpus_fname='all.txt',
+        corpus_dir_path='./corpus/Hindi',
+        need_preprocessing=True,
+        iter=5
+    )
+    generate_model(
+        language='kannada',
+        corpus_fname='all.txt',
+        corpus_dir_path='./corpus/Kannada',
+        need_preprocessing=True,
+        iter=5,
+    )
+    generate_model(
+        language='tamil',
+        corpus_fname='all.txt',
+        corpus_dir_path='./corpus/Tamil',
+        need_preprocessing=True,
+        iter=5
+    )
+    generate_model(
+        language='english',
+        corpus_fname='all.txt',
+        corpus_dir_path='./corpus/English',
+        need_preprocessing=True,
+        iter=5
+    )
