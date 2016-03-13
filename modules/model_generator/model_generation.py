554fbbddfe13cb0b79484798386b6ee5e291f50a
Kshitij Karthick <kshitij.karthick@gmail.com>
2016-03-13 15:42:33 +0530

    Individual modules setup to manage seperate functionality

--- modules/model_generator/model_generation.py
+++ modules/model_generator/model_generation.py
@@ -9,16 +9,16 @@ Used to generate Word2Vec Models for individual languages after preprocessing.
 
 import os
 import gensim
-from hccorpus_preprocessor import HcCorpusPreprocessor
+from modules.preprocessor import hccorpus_preprocessor as pre
 
 
 def generate_model(
     language,
     corpus_fname,
     corpus_dir_path='.',
-    output_dir_path='./models',
+    output_dir_path='../data/models',
     need_preprocessing=True,
-    iter=5,
+    iter=5
 ):
     """
     Function used to preprocess and generate models.
@@ -35,43 +35,51 @@ def generate_model(
     * iter               - (number)  - Number of iterations for Word2Vec
 
     """
-    preprocessor_obj = HcCorpusPreprocessor(
+    preprocessor_obj = pre.HcCorpusPreprocessor(
         corpus_fname=corpus_fname,
         corpus_dir_path=corpus_dir_path,
         need_preprocessing=True
     )
-    model = gensim.models.Word2Vec(preprocessor_obj, iter=iter)
+    return construct_model(preprocessor_obj, iter)
+
+
+def construct_model(
+    preprocessed_corpus, language, output_dir_path=".", iter=5
+):
+    """Test."""
+    model = gensim.models.Word2Vec(preprocessed_corpus, iter=iter)
     output_fname = os.path.join(output_dir_path, 't-vex-%s-model' % (language))
     model.save(output_fname)
     print("Model saved at %s" % (output_fname))
+    return model
 
 
 if __name__ == '__main__':
-    # generate_model(
-    #     language='hindi',
-    #     corpus_fname='all.txt',
-    #     corpus_dir_path='./corpus/Hindi',
-    #     need_preprocessing=True,
-    #     iter=5
-    # )
-    # generate_model(
-    #     language='kannada',
-    #     corpus_fname='all.txt',
-    #     corpus_dir_path='./corpus/Kannada',
-    #     need_preprocessing=True,
-    #     iter=5,
-    # )
-    # generate_model(
-    #     language='tamil',
-    #     corpus_fname='all.txt',
-    #     corpus_dir_path='./corpus/Tamil',
-    #     need_preprocessing=True,
-    #     iter=5
-    # )
+    generate_model(
+        language='hindi',
+        corpus_fname='all.txt',
+        corpus_dir_path='../data/corpus/Hindi',
+        need_preprocessing=True,
+        iter=5
+    )
+    generate_model(
+        language='kannada',
+        corpus_fname='all.txt',
+        corpus_dir_path='../data/corpus/Kannada',
+        need_preprocessing=True,
+        iter=5,
+    )
+    generate_model(
+        language='tamil',
+        corpus_fname='all.txt',
+        corpus_dir_path='../data/corpus/Tamil',
+        need_preprocessing=True,
+        iter=5
+    )
     generate_model(
         language='english',
         corpus_fname='all.txt',
-        corpus_dir_path='./corpus/English',
+        corpus_dir_path='../data/corpus/English',
         need_preprocessing=True,
         iter=5
     )
