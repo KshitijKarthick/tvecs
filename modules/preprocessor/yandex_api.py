fdd0fc1e5646e2a0885df9fae725b6b8ca2c4af4
Kshitij Karthick <kshitij.karthick@gmail.com>
2016-04-03 19:48:34 +0530

    Yandex: T-Vecs model score removed from score file, this is computed in evaluation

--- modules/preprocessor/yandex_api.py
+++ modules/preprocessor/yandex_api.py
@@ -8,8 +8,6 @@ import os
 import json
 import codecs
 import requests
-from gensim.models import Word2Vec
-import modules.vector_space_mapper.vector_space_mapper as vsm
 
 
 def yandex_api(lang_translate, input_score_path, output_score_path):
@@ -19,12 +17,12 @@ def yandex_api(lang_translate, input_score_path, output_score_path):
     WordSim score, translated on one column using Yandex.
     Yandex Api Key, lang for translation needs to be provided
     """
-    vm = load_vector_space_mapper()
     base_url = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
     options = {
         'lang': lang_translate,
         'key': '<Enter Key Here>'
     }
+    output_data = []
     with codecs.open(input_score_path, 'r', encoding='utf-8') as file:
         with codecs.open(output_score_path, 'w', encoding='utf-8') as outfile:
             for line in file:
@@ -34,37 +32,15 @@ def yandex_api(lang_translate, input_score_path, output_score_path):
                     requests.get(base_url, params=options).text
                 )["text"][0]
                 try:
-                    outfile.write("%s %s %s %s\n" % (
+                    output_data.append("%s %s %s" % (
                         word_1,
                         tr_word_2,
-                        score,
-                        vm.obtain_cosine_similarity(word_1, tr_word_2)
+                        score
                     ))
                 except KeyError:
                     pass
+            outfile.write("\n".join(output_data))
 
-
-def load_vector_space_mapper():
-    """Generate Vector Space Mapper."""
-    model_1 = Word2Vec.load(
-        os.path.join('data', 'models', 't-vex-english-model')
-    )
-    model_2 = Word2Vec.load(
-        os.path.join('data', 'models', 't-vex-hindi-model')
-    )
-    with codecs.open(
-        os.path.join(
-            'data', 'bilingual_dictionary', 'english_hindi_train_bd'
-        ), 'r', encoding='utf-8'
-    ) as file:
-        data = file.read().split('\n')
-        bilingual_dict = [
-            (line.split(' ')[0], line.split(' ')[1])
-            for line in data
-        ]
-        vm = vsm.VectorSpaceMapper(model_1, model_2, bilingual_dict)
-        vm.map_vector_spaces()
-    return vm
 
 if __name__ == '__main__':
 
