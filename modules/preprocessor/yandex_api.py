c6d242bd2709ecb2e4077d03bba0e688ef199e3e
Prarthana S Sannamani <ps.sannamani@gmail.com>
2016-04-06 22:55:22 +0530

    Yandex: Add a function to return a translation of a single word

--- modules/preprocessor/yandex_api.py
+++ modules/preprocessor/yandex_api.py
@@ -11,6 +11,14 @@ import json
 import codecs
 import requests
 
+def translate(word):
+    base_url = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
+    options = {
+        'lang': 'en-hi',
+        'key': '<Enter Key Here>',
+        'text': word
+    }
+    return json.loads(requests.get(base_url, params=options).text)
 
 def yandex_api(lang_translate, input_score_path, output_score_path):
     """
@@ -19,20 +27,13 @@ def yandex_api(lang_translate, input_score_path, output_score_path):
     WordSim score, translated on one column using Yandex.
     Yandex Api Key, lang for translation needs to be provided
     """
-    base_url = 'https://translate.yandex.net/api/v1.5/tr.json/translate'
-    options = {
-        'lang': lang_translate,
-        'key': '<Enter Key Here>'
-    }
     output_data = []
     with codecs.open(input_score_path, 'r', encoding='utf-8') as file:
         with codecs.open(output_score_path, 'w', encoding='utf-8') as outfile:
             for line in file:
                 word_1, word_2, score = line.split()
                 options['text'] = word_2
-                tr_word_2 = json.loads(
-                    requests.get(base_url, params=options).text
-                )["text"][0]
+                tr_word_2 = translate(word_2, lang_translate, key)["text"][0]
                 try:
                     output_data.append("%s %s %s" % (
                         word_1,
@@ -42,7 +43,6 @@ def yandex_api(lang_translate, input_score_path, output_score_path):
                 except KeyError:
                     pass
             outfile.write("\n".join(output_data))
-
 
 if __name__ == '__main__':
 
