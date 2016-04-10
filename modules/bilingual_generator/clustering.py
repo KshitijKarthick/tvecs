9a4221f2461f7da40d4fd34511478b9d64f3643d
Kshitij Karthick <kshitij.karthick@gmail.com>
2016-04-10 12:35:28 +0530

    Clustering: Update Documentation to rst for automatic autodoc support of Sphinx.
    * Modules
    	* clustering

--- modules/bilingual_generator/clustering.py
+++ modules/bilingual_generator/clustering.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python2.7
 # -*- coding: utf-8 -*-
-"""Test."""
+"""**Module generates clusters from a Model.**"""
 
 from sklearn.cluster import KMeans
 from gensim.models import Word2Vec
@@ -13,7 +13,19 @@ import numpy as np
 
 
 def cluster(language, vec_list, word_list, num_clusters=1000, n_jobs=3):
-    """Utilised for clustering passed Vector List, and return n clusters."""
+    """
+    **Utilised for clustering passed Vector List, and return n clusters.**
+
+    **API Documentation:**
+        :param language: Language utilised for filename for cluster file.
+        :param vec_list: A vector list obtained from the Model.
+        :param word_list: A word list obtained from the Model corresponding to vec_list.
+        :param num_clusters: Num of clusters to be generated [ Default 1000 ]
+        :param n_jobs: Num of jobs [ multiprocessing ]
+
+    .. seealso::
+        * :mod:`modules.model_generator.model_generation`
+    """
     kmeans_clust = KMeans(
         n_clusters=num_clusters,
         precompute_distances=True,
