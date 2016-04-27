cbf2f91f618baaa8e4410a180b4981f932fe463d
Kshitij Karthick <kshitij.karthick@gmail.com>
2016-04-27 14:40:35 +0530

    ArticleSimiliarity: Compute Avg Intercluster distance

--- modules/article_similarity/tf_idf.py
+++ modules/article_similarity/tf_idf.py
@@ -8,6 +8,7 @@ import codecs
 import itertools
 import numpy as np
 import scipy
+import json
 from sklearn.cluster import AffinityPropagation
 from gensim.models import Word2Vec
 from modules.vector_space_mapper.vector_space_mapper import VectorSpaceMapper
@@ -76,60 +77,53 @@ def top_words(scores, words, n=10):
 if __name__ == '__main__':
     kwargs = _load_models(cross_lingual=True)
     tr = get_cmp_func(**kwargs)
-    tf1 = TfidfVectorizer(input="file", min_df=1, stop_words='english')
     english_articles = glob.glob(
-        os.path.join("data", "blogs", "English") + os.path.sep + "*"
+        os.path.join("data", "blogs", "English") + os.path.sep + "*.json"
     )
-    english_articles_obj = [
-        codecs.open(f, 'r', encoding='utf-8') for f in english_articles
-    ]
     hindi_articles = glob.glob(
-        os.path.join("data", "blogs", "Hindi") + os.path.sep + "*"
+        os.path.join("data", "blogs", "Hindi") + os.path.sep + "*.json"
     )
-    tfidf_matrix1 = tf1.fit_transform(english_articles_obj)
-    dense_mat1 = tfidf_matrix1.todense().tolist()
-    for f in english_articles_obj:
-        f.close()
-    tf2 = TfidfVectorizer(input="file", min_df=1, stop_words='english')
-    hindi_articles_obj = [
-        codecs.open(f, 'r', encoding='utf-8') for f in hindi_articles
-    ]
-    tfidf_matrix2 = tf2.fit_transform(hindi_articles_obj)
-    dense_mat2 = tfidf_matrix2.todense().tolist()
-    for f in english_articles_obj:
-        f.close()
     z = {}
+    sum = 0.0
     for ((l1, l2), (index1, index2)) in zip(
             itertools.product(
-                dense_mat1, dense_mat2
+                english_articles, hindi_articles
             ), itertools.product(
                 range(1, 21), range(1, 21)
             )
     ):
-        words1 = top_words(l1, tf1.get_feature_names())
-        words2 = top_words(l2, tf2.get_feature_names())
+        cluster_list_1 = None
+        cluster_list_2 = None
+        with codecs.open(l1, 'r', encoding='utf-8') as article_1:
+            cluster_list_1 = json.load(article_1)
+        with codecs.open(l2, 'r', encoding='utf-8') as article_2:
+            cluster_list_2 = json.loads(article_2.read())
+        for cluster1, cluster2 in itertools.product(
+            cluster_list_1, cluster_list_2
+        ):
+            word_vectors1 = []
+            word_vectors2 = []
 
-        word_vectors1 = []
-        word_vectors2 = []
+            for word in cluster1:
+                try:
+                    word_vectors1.append(tr(word))
+                except:
+                    pass
 
-        for word in words1:
-            try:
-                word_vectors1.append(tr(word))
-            except:
-                pass
-
-        for word in words2:
-            try:
-                word_vectors2.append(kwargs['model_2'][word])
-            except:
-                pass
-        distance = scipy.spatial.distance.euclidean(
-            np.mean(word_vectors1, axis=0),
-            np.mean(word_vectors2, axis=0)
-        )
+            for word in cluster2:
+                try:
+                    word_vectors2.append(kwargs['model_2'][word])
+                except:
+                    pass
+            distance = scipy.spatial.distance.euclidean(
+                np.mean(word_vectors1, axis=0),
+                np.mean(word_vectors2, axis=0)
+            )
+            sum += distance
         z[
             (english_articles[index1 - 1], hindi_articles[index2 - 1])
-        ] = distance
+        ] = sum / (len(cluster_list_1) * len(cluster_list_2))
+        sum = 0.0
 
     l = sorted(z.items(), key=lambda (x, y): y, reverse=True)
     for x in l:
