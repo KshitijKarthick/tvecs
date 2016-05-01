165673acd33f7bcb54322e1285a93aa12831f3e5
Kshitij Karthick <kshitij.karthick@gmail.com>
2016-05-01 14:15:34 +0530

    Sphinx: Add Documentation for Unit Test cases for Emille Preprocessor

--- tests/test_emille_preprocessor.py
+++ tests/test_emille_preprocessor.py
@@ -6,25 +6,57 @@ import pytest
 
 from tvecs.preprocessor.emille_preprocessor import EmilleCorpusPreprocessor
 
+
 class TestEmilleCorpusPreprocessor:
+    """
+    Unit Testing for module :mod:`modules.preprocessor.emille_preprocessor`.
+
+    .. seealso::
+        * :mod:`modules.preprocessor.base_preprocessor`
+        * :mod:`pytest`
+    """
 
     def setup_class(cls):
-        cls.testing_obj = EmilleCorpusPreprocessor(
-            corpus_fname='test_emille_corpus',
-            corpus_dir_path=os.path.join('tests', 'resources'),
-            need_preprocessing=True
-        )
+        """
+        Create instance of :class:`modules.preprocessor.emille_preprocessor.EmilleCorpusPreprocessor`
+
+        API Documentation:
+            :param cls: Class level scope
+            :type cls: :class:`TestEmilleCorpusPreprocessor`
+        """
+        try:
+            cls.testing_obj = EmilleCorpusPreprocessor(
+                corpus_fname='test_emille_corpus',
+                corpus_dir_path=os.path.join('tests', 'resources'),
+                need_preprocessing=True
+            )
+        except BaseException as err:
+            pytest.fail(
+                'Pre-processing %s failed : %s' % ('test_emille_corpus', err.message)
+            )
 
     def teardown_class(cls):
+        """
+        Delete the preprocessed files generated.
+
+        API Documentation:
+            :param cls: Class level scope
+            :type cls: :class:`TestEmilleCorpusPreprocessor`
+        """
         try:
             os.remove(
                 os.path.join('tests', 'resources', 'test_emille_corpus.%s' % 'preprocessed')
             )
-        except (OSError, IOError) as err:
+        except (OSError, IOError):
             pass
         pass
 
     def test_implement_extract_corpus_data(self):
+        """
+        Test :func:`modules.preprocessor.emille_preprocessor.EmilleCorpusPreprocessor._extract_corpus_data`
+
+        Ensure that the function is implemented in the class.
+        """
         testing_object = self.__class__.testing_obj
         try:
             testing_object._extract_corpus_data(data='')
@@ -32,6 +64,11 @@ class TestEmilleCorpusPreprocessor:
             pytest.fail('Not Implemented _extract_corpus_data function')
 
     def test_implement_clean_word(self):
+        """
+        Test :func:`modules.preprocessor.emille_preprocessor.EmilleCorpusPreprocessor._clean_word`
+
+        Ensure that the function is implemented in the class.
+        """
         testing_object = self.__class__.testing_obj
         try:
             testing_object._clean_word(word='')
@@ -39,6 +76,11 @@ class TestEmilleCorpusPreprocessor:
             pytest.fail('Not Implemented _clean_word function')
 
     def test_implement_tokenize_sentences(self):
+        """
+        Test :func:`modules.preprocessor.emille_preprocessor.EmilleCorpusPreprocessor._tokenize_sentences`
+
+        Ensure that the function is implemented in the class.
+        """
         testing_object = self.__class__.testing_obj
         try:
             testing_object._tokenize_sentences(data='')
@@ -46,6 +88,11 @@ class TestEmilleCorpusPreprocessor:
             pytest.fail('Not Implemented _tokenize_sentences function')
 
     def test_implement_tokenize_words(self):
+        """
+        Test :func:`modules.preprocessor.emille_preprocessor.EmilleCorpusPreprocessor._tokenize_words`
+
+        Ensure that the function is implemented in the class.
+        """
         testing_object = self.__class__.testing_obj
         try:
             testing_object._tokenize_words(sentence='')
@@ -53,6 +100,11 @@ class TestEmilleCorpusPreprocessor:
             pytest.fail('Not Implemented _tokenize_words function')
 
     def test_english_tokenize_sentences(self):
+        """
+        Test :func:`modules.preprocessor.emille_preprocessor.EmilleCorpusPreprocessor._tokenize_sentences`.
+
+        Ensure sentence tokenizer for english works as expected.
+        """
         testing_object = self.__class__.testing_obj
         data = 'We love you Mr. Brown.. If I were a bear,'
         expected = ['We love you Mr. Brown..', 'If I were a bear,']
@@ -60,6 +112,11 @@ class TestEmilleCorpusPreprocessor:
         assert expected == result, "_tokenize_sentences failed for english language"
 
     def test_hindi_tokenize_sentences(self):
+        """
+        Test :func:`modules.preprocessor.emille_preprocessor.EmilleCorpusPreprocessor._tokenize_sentences`.
+
+        Ensure that the sentence tokenizer for hindi works as expected.
+        """
         testing_object = self.__class__.testing_obj
         data = u"ये कहानी तो मेरे को मेरे नाना ने सुनाई थी. गरमी का मौसम था ….." \
                u" सभी नदियाँ, तालाब , कुँए सूख गये थे. पानी का नामोनिशान तक ना था." \
@@ -90,6 +147,11 @@ class TestEmilleCorpusPreprocessor:
             pytest.fail("_tokenize_sentences failed for hindi language : %s" % err.message)
 
     def test_tokenize_words(self):
+        """
+        Test :func:`modules.preprocessor.emille_preprocessor.EmilleCorpusPreprocessor._tokenize_sentences`.
+
+        Ensure that the word tokenizer for hindi works as expected.
+        """
         testing_object = self.__class__.testing_obj
         data = u'मैं आसपास बिखरे पड़े कंकड़-पत्थर चोंच से ला-लाकर घड़े में डालने लगा बस क्या था !'
         expected = [
@@ -100,6 +162,11 @@ class TestEmilleCorpusPreprocessor:
         assert result == expected, "_tokenize_words failed"
 
     def test_hindi_clean_word(self):
+        """
+        Test :func:`modules.preprocessor.emille_preprocessor.EmilleCorpusPreprocessor._clean_word`.
+
+        Ensure that the word level clean functionality works for hindi.
+        """
         testing_object = self.__class__.testing_obj
         data = [
             u'मैं', u'आसपास', u'बिखरे', u'पड़े', u'कंकड़-पत्थर,', u'चोंच', u'से',
@@ -107,7 +174,7 @@ class TestEmilleCorpusPreprocessor:
         ]
         expected = [
             [u'मैं'], [u'आसपास'], [u'बिखरे'], [u'पड़े'], [u'कंकड़', u'पत्थर'], [u'चोंच'], [u'से'],
-            [u'ला', u'लाकर'], [u'घड़े'], [u'में'], [],[u'डालने'], [u'लगा'], [u'बस'], [u'क्या'], [u'था'], []
+            [u'ला', u'लाकर'], [u'घड़े'], [u'में'], [], [u'डालने'], [u'लगा'], [u'बस'], [u'क्या'], [u'था'], []
         ]
         testing_object.language = 'hindi'
         for i in range(len(data)):
@@ -116,6 +183,11 @@ class TestEmilleCorpusPreprocessor:
         testing_object.language = 'english'
 
     def test_english_clean_word(self):
+        """
+        Test :func:`modules.preprocessor.emille_preprocessor.EmilleCorpusPreprocessor._clean_word`.
+
+        Ensure that the word level clean functionality works for english.
+        """
         testing_object = self.__class__.testing_obj
         data = [
             'Typed', 'essays', 'are', 'preferred', 'but',
