5ff34aff434de3334b1b0a31fad361c9648a3488
Kshitij Karthick <kshitij.karthick@gmail.com>
2016-05-01 14:17:14 +0530

    Sphinx: Add Documentation for Unit Test cases for Leipzig Preprocessor

--- tests/test_leipzig_preprocessor.py
+++ tests/test_leipzig_preprocessor.py
@@ -6,9 +6,24 @@ import pytest
 
 from tvecs.preprocessor.leipzig_preprocessor import LeipzigPreprocessor
 
-class TestHcCorpusPreprocessor:
+
+class TestLeipzigPreprocessor:
+    """
+    Unit Testing for module :mod:`modules.preprocessor.leipzig_preprocessor`.
+
+    .. seealso::
+        * :mod:`modules.preprocessor.base_preprocessor`
+        * :mod:`pytest`
+    """
 
     def setup_class(cls):
+        """
+        Create instance of :class:`modules.preprocessor.leipzig_preprocessor.LeipzigPreprocessor`
+
+        API Documentation:
+            :param cls: Class level scope
+            :type cls: :class:`TestLeipzigPreprocessor`
+        """
         try:
             cls.testing_obj = LeipzigPreprocessor(
                 corpus_fname='test_leipzig_corpus',
@@ -16,18 +31,30 @@ class TestHcCorpusPreprocessor:
             )
         except BaseException as err:
             pytest.fail(
-                'Pre-processing %s failed : %s' %('test_leipzig_corpus', err.message)
+                'Pre-processing %s failed : %s' % ('test_leipzig_corpus', err.message)
             )
 
     def teardown_class(cls):
+        """
+        Delete the preprocessed files generated.
+
+        API Documentation:
+            :param cls: Class level scope
+            :type cls: :class:`TestLeipzigPreprocessor`
+        """
         try:
             os.remove(
                 os.path.join('tests', 'resources', 'test_leipzig_corpus.%s' % 'preprocessed')
             )
-        except (OSError, IOError) as err:
+        except (OSError, IOError):
             pass
 
     def test_implement_clean_word(self):
+        """
+        Test :func:`modules.preprocessor.leipzig_preprocessor.LeipzigPreprocessor._clean_word`
+
+        Ensure that the function is implemented in the class.
+        """
         testing_object = self.__class__.testing_obj
         try:
             testing_object._clean_word(word='')
@@ -35,6 +62,11 @@ class TestHcCorpusPreprocessor:
             pytest.fail('Not Implemented _clean_word function')
 
     def test_implement_tokenize_words(self):
+        """
+        Test :func:`modules.preprocessor.leipzig_preprocessor.LeipzigPreprocessor._tokenize_words`
+
+        Ensure that the function is implemented in the class.
+        """
         testing_object = self.__class__.testing_obj
         try:
             testing_object._tokenize_words(sentence='')
@@ -42,6 +74,11 @@ class TestHcCorpusPreprocessor:
             pytest.fail('Not Implemented _tokenize_words function')
 
     def test_tokenize_words(self):
+        """
+        Test :func:`modules.preprocessor.leipzig_preprocessor.LeipzigPreprocessor._tokenize_sentences`.
+
+        Ensure that the sentence tokenizer for hindi works as expected.
+        """
         testing_object = self.__class__.testing_obj
         data = u'मैं आसपास बिखरे पड़े कंकड़-पत्थर चोंच से ला-लाकर घड़े में डालने लगा बस क्या था !'
         expected = [
@@ -52,6 +89,11 @@ class TestHcCorpusPreprocessor:
         assert result == expected, "_tokenize_words failed"
 
     def test_hindi_clean_word(self):
+        """
+        Test :func:`modules.preprocessor.leipzig_preprocessor.LeipzigPreprocessor._clean_word`.
+
+        Ensure that the word level clean functionality works for hindi.
+        """
         testing_object = self.__class__.testing_obj
         data = [
             u'मैं', u'आसपास', u'बिखरे', u'पड़े', u'कंकड़-पत्थर,', u'चोंच', u'से',
@@ -59,7 +101,7 @@ class TestHcCorpusPreprocessor:
         ]
         expected = [
             [u'मैं'], [u'आसपास'], [u'बिखरे'], [u'पड़े'], [u'कंकड़', u'पत्थर'], [u'चोंच'], [u'से'],
-            [u'ला', u'लाकर'], [u'घड़े'], [u'में'], [],[u'डालने'], [u'लगा'], [u'बस'], [u'क्या'], [u'था'], []
+            [u'ला', u'लाकर'], [u'घड़े'], [u'में'], [], [u'डालने'], [u'लगा'], [u'बस'], [u'क्या'], [u'था'], []
         ]
         testing_object.language = 'hindi'
         for i in range(len(data)):
@@ -68,6 +110,11 @@ class TestHcCorpusPreprocessor:
         testing_object.language = 'english'
 
     def test_english_clean_word(self):
+        """
+        Test :func:`modules.preprocessor.leipzig_preprocessor.LeipzigPreprocessor._clean_word`.
+
+        Ensure that the word level clean functionality works for english.
+        """
         testing_object = self.__class__.testing_obj
         data = [
             'Typed', 'essays', 'are', 'preferred', 'but',
