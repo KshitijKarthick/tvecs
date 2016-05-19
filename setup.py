b4a56534185857e1709c23fe2315f7f6b819ed8c
Kshitij Karthick <kshitij.karthick@gmail.com>
2016-05-19 10:15:24 +0530

    Setuptools: Update requirements

--- setup.py
+++ setup.py
@@ -34,13 +34,13 @@ setup(
     setup_requires=['pytest-runner'],
     tests_require=['pytest'],
     install_requires=[
-        'gensim >= 0.12.4',
-        'nltk >= 3.1',
-        'regex >= 2016.3.2',
-        'scipy >= 0.14.0',
-        'scikit-learn >= 0.15.2'
-        'beautifulsoup4 >= 4.4.1',
-        'pytest >= 2.9.1'
+        'gensim>=0.12.4',
+        'nltk>=3.1',
+        'regex>=2016.3.2',
+        'scipy>=0.14.0',
+        'scikit-learn>=0.15.2'
+        'beautifulsoup4>=4.4.1',
+        'pytest>=2.9.1'
     ],
     use_2to3=True,
     classifiers=[
